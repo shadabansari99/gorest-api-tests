6f438a37458138c4fd2c6e868cd8012cd3310e78
Shadab Ansari <shadaba@testvagrant.com>
2022-10-03 12:53:32 +0530

    Added gorest testcase

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -1,13 +1,26 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+
 public class SampleTests {
     @Test
-    public void shouldTestSomething()
+    public void shouldGetAllUsers()
     {
-        Assert.assertEquals(1,1);
-        //add
-        //sub
+        //Arrange
+
+        //Act
+
+        //Assert
+        given()
+            .when()
+                .get("https://gorest.co.in/public/v2/users")
+            .then()
+                .statusCode(200)
+                .log()
+                .body();
+
     }
 
 }
